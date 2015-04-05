bf5da0a46949deaa37c21958c2aa0b150374f496
Nguyễn Lê Thành Dũng <nltd@nguyentito.eu>
2015-04-05 04:38:48 +0200

    move action_id from actions.hh to constant.hh

--- src/actions.hh
+++ src/actions.hh
@@ -9,14 +9,4 @@
 #include "action_utiliser_virus.hh"
 #include "action_utiliser_turbo.hh"
 
-enum action_id {
-    ID_ACTION_CAPTURER,
-    ID_ACTION_LIER,
-    ID_ACTION_DETRUIRE,
-    ID_ACTION_DEPLACER,
-    ID_ACTION_AJOUTER_BOUCLIER,
-    ID_ACTION_UTILISER_VIRUS,
-    ID_ACTION_UTILISER_TURBO
-};
-
 #endif // !ACTIONS_HH
