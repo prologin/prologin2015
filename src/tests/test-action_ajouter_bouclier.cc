752a8b81f62b06af056c435eeea291d4452b8f10
Sacha Delanoue <assos@shaac.me>
2015-04-06 15:46:09 +0200

    Add test for action points when adding shield

---
+++ src/tests/test-action_ajouter_bouclier.cc
@@ -0,0 +1,29 @@
+#include "../action_ajouter_bouclier.hh"
+#include "test-helpers.hh"
+
+// All the tests below are methods coming from the ActionTest class (see
+// test-helpers.hh).  This is where the "st" GameState is coming from.
+
+// Set a given number of action points to a given player
+static inline void set_points(GameState* st, int player, int pts)
+{
+    st->decrement_action_points(player, st->action_points(player) - pts);
+}
+
+// Test that player has sufficient action points for the action
+TEST_F(ActionTest, ActionAjouterBouclier_TooFewActionPoints)
+{
+    for (int player : {PLAYER_1, PLAYER_2})
+    {
+        // Move this player on an owned portal and create action
+        st->set_pos(player, st->portal_pos(0));
+        st->capture(0, player);
+        ActionAjouterBouclier action(player);
+
+        set_points(st, player, COUT_BOUCLIER - 1);
+        EXPECT_EQ(PA_INSUFFISANTS, action.check(st));
+
+        set_points(st, player, COUT_BOUCLIER);
+        EXPECT_NE(PA_INSUFFISANTS, action.check(st));
+    }
+}
