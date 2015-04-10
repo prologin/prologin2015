36c54d24dfccd6d80be39ec7821c24f45d7d164e
Pierre-Marie de Rodat <pmderodat@kawie.fr>
2015-04-11 01:01:07 +0200

    GameState: add comments to document methods/fields

--- src/game_state.hh
+++ src/game_state.hh
@@ -13,13 +13,24 @@
 #include "map.hh"
 #include "graph.hh"
 
+
+// Additionnal information for players to complete Stechec player data
+// structures.
 struct player_info
 {
+    // Action an move points still available for the player for the current
+    // turn.
     int action_points;
     int move_points;
+
+    // Position of the player's agent.
     position pos;
+
+    // Reference to the score in Stechec's player data structures, for
+    // convenience.
     int *score;
 };
+
 
 class GameState : public rules::GameState
 {
@@ -32,56 +43,104 @@ public:
     virtual rules::GameState* copy() const;
     ~GameState();
 
+    // Return the current turn number.
     int get_current_turn() const;
+    // Increment the current turn number.  This just increments the "turn"
+    // counter. On the other hand, it does not currently reset other counters
+    // such as players' action/move points.
     void increment_turn();
 
+    // Return false when it's still time for players to play, true otherwise.
     bool is_finished() const;
 
+    // Accessor for Stechec players, used in the dumper.
     rules::Players_sptr get_players() const { return players_; }
+    // Return the ID for "player_id"'s opponent player.
     int get_opponent(int player_id) const;
+    // Return the score for "player_id".
     int get_score(int player_id) const;
+    // Add "delta" to "player_id"'s score ("delta" can be negative).
     void increment_score(int player_id, int delta);
 
-    const Graph& graph() const { return graph_; }
-    Graph& graph() { return graph_; }
+    // Accessor for the map.
     const Map& map() const { return *map_; }
     Map& map() { return *map_; }
+    // Accessor the graph for all edges/triangles created by players.
+    const Graph& graph() const { return graph_; }
+    Graph& graph() { return graph_; }
 
+    // Return the player ID for the player that owns a portal.  Return -1 if
+    // the portal is not owned by anyone.
     int owner(int portal_id) const;
+    // Remove any ownership for some portal.  This also removes shields for
+    // this portal.
     void neutralize(int portal_id);
+    // Make "player_id" own "portal_id".  This also removes shields for this
+    // portal.
     void capture(int portal_id, int player_id);
 
+    // Return the owner of a link (edge) and of a field (triangle). Note that
+    // it's the same as the owner of any involved portal, so these are
+    // shortcuts.
     int owner(const ipair& link) const;
     int owner(const itriple& field) const;
+
+    // Extract information from edges/triangles in order to produce the "lien"
+    // and "champ" data structures.
     lien edge_to_link(const ipair& e) const;
     champ triangle_to_field(const itriple& t) const;
 
+    // Helper to unpack triangles into portals.
     void unpack_triangle_pos(const itriple& t,
                              position& a, position& b, position& c) const;
+    // TODO: I guess this computes twice the number of cells covered by some
+    // field?
     int field_area_x2(const itriple& field) const;
-    
+
+    // Return the number of shieleds associated to "portal_id".
     int num_shields(int portal_id) const;
+    // Increment the of shields associated to "portal_id".
     void add_shield(int portal_id);
 
+    // Return the number of action points still available for "player_id" for
+    // the current turn.
     int action_points(int player_id) const;
+    // Likewise for move points.
     int move_points(int player_id) const;
+
+    // Decrement the number of action points still available for "player_id"
+    // for the current turn by "delta".
     void decrement_action_points(int player_id, int delta);
+    // Likewise for move points.
     void increment_move_points(int player_id, int delta);
 
+    // Return the position for some portal.
     const position& portal_pos(int portal_id) const;
+    // Return the position for some player.
     const position& player_pos(int player_id) const;
+    // Move the agent for "player_id" to some position. This does _not_ update
+    // the corresponding move points.
     void set_pos(int player_id, const position& position);
 
 private:
+    // Array of all clients on this game.  Note that this also contains
+    // spectators.
     rules::Players_sptr players_;
+
     int current_turn_;
 
     std::shared_ptr<Map> map_;
     Graph graph_;
 
+    // Mapping: portal ID -> ID for the player that owns the corresponding
+    // portal, or -1 if no one owns it.
     std::vector<int> portal_player_;
+
+    // Mapping: portal ID -> number of shields on the corresponding portal.
     std::vector<int8_t> portal_shields_;
 
+    // Mapping: player ID -> player_info data structure.  Spectators are
+    // missing from this mapping.
     std::map<int, player_info> player_info_;
 };
 
