cd038720ff5ca9afb24e7d14abc50324fd6e81bb
Nguyễn Lê Thành Dũng <nltd@nguyentito.eu>
2015-04-13 16:45:31 +0200

    forgot to commit action_neutraliser in autogen

---
+++ src/action_neutraliser.cc
@@ -0,0 +1,12 @@
+#include "actions.hh"
+
+int ActionNeutraliser::check(const GameState* st) const
+{
+    // FIXME
+    return 0;
+}
+
+void ActionNeutraliser::apply_on(GameState* st) const
+{
+    // FIXME
+}
---
+++ src/action_neutraliser.hh
@@ -0,0 +1,31 @@
+#ifndef ACTION_NEUTRALISER_HH
+#define ACTION_NEUTRALISER_HH
+
+#include <rules/action.hh>
+
+#include "actions.hh"
+#include "game_state.hh"
+#include "constant.hh"
+
+class ActionNeutraliser : public rules::Action<GameState>
+{
+public:
+    ActionNeutraliser(int player_id) : player_id_(player_id) {}
+    ActionNeutraliser() {} // for register_action()
+
+    virtual int check(const GameState* st) const;
+    virtual void apply_on(GameState* st) const;
+
+    virtual void handle_buffer(utils::Buffer& buf)
+    {
+        buf.handle(player_id_);
+    }
+
+    uint32_t player_id() const { return player_id_; };
+    uint32_t id() const { return ID_ACTION_NEUTRALISER; }
+
+private:
+    int player_id_;
+};
+
+#endif // !ACTION_NEUTRALISER_HH
