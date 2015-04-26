1a8bf034237d924d2cb4248c22ec60ff41a09d54
Nguyễn Lê Thành Dũng <nltd@nguyentito.eu>
2015-04-26 02:49:56 +0200

    More unit tests for action_lier

--- src/tests/test-action_lier.cc
+++ src/tests/test-action_lier.cc
@@ -23,7 +23,17 @@ TEST_F(ActionTest, ActionLier_TooFewActionPoints)
     }
 }
 
-// Test that action do not work when the player is not on a portal
+// Test that the action properly behaves with an invalid position.
+TEST_F(ActionTest, Deplacer_InvalidPos)
+{
+    ActionLier action({-1, 0}, PLAYER_1);
+    st->set_pos(PLAYER_1, {0, 0});
+
+    EXPECT_EQ(POSITION_INVALIDE, action.check(st));
+}
+
+
+// Test that the action does not work when the player is not on a portal
 TEST_F(ActionTest, ActionLier_NoPortalHere)
 {
     for (int player : {PLAYER_1, PLAYER_2})
@@ -41,7 +51,7 @@ TEST_F(ActionTest, ActionLier_NoPortalHere)
     }
 }
 
-// Test that action do not work when there is no portal on the target destination
+// Test that the action does not work when there is no portal on the target destination
 TEST_F(ActionTest, ActionLier_NoPortalThere)
 {
     for (int player : {PLAYER_1, PLAYER_2})
@@ -56,7 +66,7 @@ TEST_F(ActionTest, ActionLier_NoPortalThere)
     }
 }
 
-// Test that action fails when on a neutral portal (on either endpoint)
+// Test that the action fails when on a neutral portal (on either endpoint)
 TEST_F(ActionTest, ActionLier_NeutralPortal)
 {
     for (int player : {PLAYER_1, PLAYER_2})
@@ -79,7 +89,7 @@ TEST_F(ActionTest, ActionLier_NeutralPortal)
     }
 }
 
-// Test that action fails when on an enemy portal (on either endpoint)
+// Test that the action fails when on an enemy portal (on either endpoint)
 TEST_F(ActionTest, ActionLier_EnemyPortal)
 {
     for (int player : {PLAYER_1, PLAYER_2})
@@ -101,6 +111,71 @@ TEST_F(ActionTest, ActionLier_EnemyPortal)
         EXPECT_NE(PORTAIL_ENNEMI, action.check(st));
     }
 }
+
+// Test that creating a degenerate link is impossible
+TEST_F(ActionTest, ActionLier_Degenerate)
+{
+    for (int player : {PLAYER_1, PLAYER_2})
+    {
+        set_points(st, player, 2 * COUT_LIEN);
+        st->set_pos(player, st->portal_pos(0));
+        ActionLier action(st->portal_pos(0), player);
+
+        st->capture(0, player);
+        EXPECT_EQ(LIEN_DEGENERE, action.check(st));
+    }
+}
+
+// Test that creating the same link twice is impossible
+TEST_F(ActionTest, ActionLier_SameLinkTwice)
+{
+    for (int player : {PLAYER_1, PLAYER_2})
+    {
+        // Put the player in a correct state for creating a link
+        const int initial_AP = COUT_LIEN + 1;
+        set_points(st, player, initial_AP);
+
+        st->capture(0, player);
+        st->capture(1, player);
+        st->set_pos(player, st->portal_pos(0));
+        ActionLier action(st->portal_pos(1), player);
+
+        // Create first link
+        EXPECT_EQ(OK, action.check(st));
+        action.apply_on(st);
+
+        // Try to create again
+        EXPECT_EQ(LIEN_INTERSECTION, action.check(st));
+    }
+}
+
+// Test that creating crossing links is impossible
+TEST_F(ActionTest, ActionLier_Crossing)
+{
+    for (int player : {PLAYER_1, PLAYER_2})
+    {
+        const int initial_AP = 2 * COUT_LIEN;
+        set_points(st, player, initial_AP);
+
+        // Create first link
+        st->capture(0, player);
+        st->capture(3, player);
+        st->set_pos(player, st->portal_pos(0));
+        ActionLier action(st->portal_pos(3), player);
+        EXPECT_EQ(OK, action.check(st));
+        action.apply_on(st);
+
+        // The player should still have enough AP to create a second link
+        st->capture(1, player);
+        st->capture(2, player);
+        st->set_pos(player, st->portal_pos(1));
+        ActionLier action2(st->portal_pos(2), player);
+
+        // Now, it should fail
+        EXPECT_EQ(LIEN_INTERSECTION, action2.check(st));
+    }
+}
+
 
 // Test that a regular action just does it job
 TEST_F(ActionTest, ActionLier_RegularOK)
