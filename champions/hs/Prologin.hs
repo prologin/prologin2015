67f263ec03e5a04eb3a3c23e62701df267fba868
Nguyễn Lê Thành Dũng <nltd@nguyentito.eu>
2015-04-29 02:53:09 +0200

    a Haskell AI which sort of works? dunno really

--- champions/hs/Prologin.hs
+++ champions/hs/Prologin.hs
@@ -6,20 +6,51 @@
 
 module Prologin where
 
+import Control.Applicative
+import Control.Monad
+import Data.List
+import System.IO.Unsafe
+-- import System.Random
+
 import Api
 
+pickRandom :: [a] -> IO a
+--pickRandom xs = (xs !!) <$> randomRIO (0, length xs - 1)
+pickRandom = return . head
+
+distance_ a b = unsafePerformIO $ distance a b
+
+nbTurbos = nb_points_action `div` (cout_turbo - 1)
+deplacement = nb_points_deplacement + nbTurbos * gain_turbo
 
 -- | Fonction appelée au début de la partie
 partie_init :: IO ()
-partie_init = return () -- A completer
+partie_init = return () -- no need to init random generator
 
 -- | Fonction appelée à chaque tour
 jouer_tour :: IO ()
-jouer_tour = return () -- A completer
+jouer_tour = do
+  portals <- liste_portails
+  replicateM_ nbTurbos utiliser_turbo
+  let loop = do
+        myPos <- position_agent =<< moi
+        putStr "my position is "
+        print myPos
+        movePts <- points_deplacement
+        case filter ((<= movePts) . distance_ myPos) . delete myPos $ portals of
+          [] -> return ()
+          orbit -> do
+            randPortal <- pickRandom orbit
+            putStrLn $ "moving to " ++ show randPortal
+            print =<< deplacer randPortal
+            print =<< capturer
+            print =<< lier =<< pickRandom portals
+            loop
+  loop
 
 -- | Fonction appelée à la fin de la partie
 partie_fin :: IO ()
-partie_fin = return () -- A completer
+partie_fin = return ()
 
 
 hs_partie_init = partie_init
