7732a651aebc67712a2c1519fac8ccaa34b969d4
Pierre-Marie de Rodat <pmderodat@kawie.fr>
2015-04-29 08:11:26 +0200

    GUI: strip unused options

--- spectators/gui/settings.py
+++ spectators/gui/settings.py
@@ -4,27 +4,16 @@ import utils
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '-w', '--width', dest='width', type=int, default=800,
-    help='Set the width of the GUI'
-)
-parser.add_argument(
-    '-H', '--height', dest='height', type=int, default=600,
-    help='Set the height of the GUI'
-)
-
-parser.add_argument(
     '-F', '--fullscreen', action='store_true', help='Fullscreen mode'
 )
-
 parser.add_argument(
     '-S', '--tv-show', action='store_true',
     help='Enables the TV show mode: display special screens and run everything'
          ' automatically (including stopping the main loop at the end)'
 )
 
+options = None
+
 def load(argv):
     global options
     options = parser.parse_args(argv)
-
-    options.width = utils.set_between(options.width, lower=500)
-    options.height = utils.set_between(options.height, lower=400)
